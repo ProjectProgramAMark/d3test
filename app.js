982d01a0c9692ecad703824e67e1c1edac5f4d5f
ProjectProgramAMark <projectprogramamark@gmail.com>
2016-07-08 17:55:14 -0400

    Dynamic link addition, moved script to external js file eukaryotes.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,8 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// app.get('/', function(req, res) {
+//     res.sendFile('index.html');
+// });
+
 app.get('/', function(req, res) {
-    res.sendFile('index.html');
+  res.render('index');
 });
 
 // catch 404 and forward to error handler
